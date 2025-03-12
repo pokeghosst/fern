a60a583def0ad0cc566cda2acc5779a8714a1577
pokeghost <pokeghost@disroot.org>
2025-03-12 18:48:27 +0200

    fix: Switch to another LZMA library

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import LZMA from 'lzma-web'
+import { compress, decompress } from 'lzma1'
 import { decodeFromBase64, encodeToBase64 } from './base64'
 
 import './style.css'
-
-const lzma = new LZMA()
 
 const params = new URLSearchParams(window.location.search)
 const pasteForm = document.getElementById('pasteForm') as HTMLFormElement
@@ -22,17 +20,12 @@ if (pasteContents) {
 
     const pasteParagraph = document.createElement('p')
     const decodedBytes = decodeFromBase64(pasteContents)
-    const decompressedString = await lzma.decompress(decodedBytes)
 
-    if (typeof decompressedString !== 'string') {
-        alert('Result is not a string. Is the paste a valid UTF-8 text?')
-    } else {
-        pasteParagraph.textContent = decompressedString
-        pasteContainer.appendChild(pasteParagraph)
-    }
+    pasteParagraph.textContent = decompress(decodedBytes) as string
+    pasteContainer.appendChild(pasteParagraph)
 }
 
-pasteForm.addEventListener('submit', async (e) => {
+pasteForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const formData = new FormData(pasteForm)
@@ -40,13 +33,11 @@ pasteForm.addEventListener('submit', async (e) => {
 
     if (!paste) return
 
-    const compressedBytes = await lzma.compress(paste.toString())
-    // APPARENTLY the result of `compress` is not `Uint8Array` since it has negative values.
-    // That's why we need to convert it to actual `Uint8Array`
-    const encodedPaste = encodeToBase64(new Uint8Array(compressedBytes))
+    const compressedBytes = compress(paste.toString())
+    const encodedCompressed = encodeToBase64(new Uint8Array(compressedBytes))
 
     const params = new URLSearchParams(window.location.search)
-    params.set('paste', encodedPaste)
+    params.set('paste', encodedCompressed)
 
     window.location.search = params.toString()
 })
