280b409bf79532de011e1f5df1d7928f680f66bc
pokeghost <pokeghost@disroot.org>
2025-11-02 14:44:12 +0200

    feat: Show version from package.json in UI

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
+import { defineConfig } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+import packageJson from './package.json'
 
 export default defineConfig({
-  plugins: [viteSingleFile()],
-});
+    plugins: [
+        viteSingleFile(),
+        {
+            name: 'html-transform',
+            transformIndexHtml(html) {
+                return html.replace('%PACKAGE_VERSION%', packageJson.version)
+            },
+        },
+    ],
+})
